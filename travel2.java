f00126c4060b42f34cb92bd115dcdeb1526f6b8c
Radhika111k <kolapradhika111@gmail.com>
2022-10-30 18:53:54 +0530

    done

--- travel2.java
+++ travel2.java
@@ -2,6 +2,6 @@ public class travel2{
 public static void main(String [] args)
 {
 System.out.println("Hello World");
-
+System.out.println("we travel not to escape life but for life not to escape us");
 }
 }
